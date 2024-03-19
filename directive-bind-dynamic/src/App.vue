aea4c89b90bf492930abd6fb8f12c0e992953e18
kota_shiratori <swan.kouta@gmail.com>
2024-03-19 15:53:51 +0900

    🎨 動的引数(Dynamic Arguments)

---
+++ directive-bind-dynamic/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const widthOrHeight = ref("height");
+
+const widthOrHeightValue = ref(600);
+</script>
+
+<template>
+  <p>
+    <img src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue" />
+  </p>
+</template>
