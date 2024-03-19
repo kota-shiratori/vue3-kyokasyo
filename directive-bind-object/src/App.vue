2dbdbcfacbabfaf9b42ea8beb96be4f53a68b7f8
kota_shiratori <swan.kouta@gmail.com>
2024-03-19 16:14:04 +0900

    🎨 複数の属性にまとめてバインド

---
+++ directive-bind-object/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const imgAttributes = ref({
+  src: "/images/src/assets/logo.svg",
+  alt: "Vue logo",
+  width: 75,
+  height: 75
+});
+</script>
+
+<template>
+  <p>
+    <img v-bind="imgAttributes">
+  </p>
+  <p>
+    <img v-bind="imgAttributes" title="ロゴです！">
+  </p>
+  <p>
+    <img v-bind="imgAttributes" alt="ロゴです！">
+  </p>
+</template>
