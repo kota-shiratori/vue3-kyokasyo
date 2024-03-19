9dad6fa8734339437294097e045497a76413b19d
kota_shiratori <swan.kouta@gmail.com>
2024-03-19 12:54:01 +0900

    🎨 computed,reactive

---
+++ computed/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const radiusInit = Math.round(Math.random() * 10);
+
+const PI = ref(3.14);
+
+const radius = ref(radiusInit);
+
+const area = computed(() => {
+  return radius.value * radius.value * PI.value;
+});
+
+setInterval((): void => {
+  radius.value = Math.round(Math.random() * 10);
+}, 1000);
+</script>
+
+<template>
+  <p>半径{{ radius }}の円の面積を円周率で計算すると、{{ area }}
+  </p>
+</template>
---
+++ reactive-function/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { reactive, computed, ref } from 'vue';
+
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+});
+
+const area = computed(() => {
+  return data.radius * data.radius * data.PI;
+});
+
+setInterval((): void => {
+  data.radius = Math.round(Math.random() * 10);
+}, 1000);
+</script>
+
+<template>
+  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}
+  </p>
+</template>
