9f221775eb0afda95159371c8364887e2627743d
kota_shiratori <swan.kouta@gmail.com>
2024-03-26 22:58:56 +0900

    🎨 イベントハンドラ以外を引数とするイベントハンドラメソッド

---
+++ directive-on-param-others/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const pBgcolor = ref("white");
+const onPClick = (bgColor: string): void => {
+  pBgcolor.value = bgColor;
+};
+</script>
+
+<template>
+  <p v-on:click="onPClick('red')" v-bind:style="{ backgroundColor: pBgcolor }">ここをクリックすると背景色が変わります。</p>
+</template>
