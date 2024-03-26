5c923cf1a2843969a57e36550e95921b88c49b41
kota_shiratori <swan.kouta@gmail.com>
2024-03-27 00:02:23 +0900

    🎨 クリックイベントとキーイベントの修飾子

---
+++ directive-on-modifiers/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { MagicString } from 'vue/compiler-sfc';
+
+const msg = ref("まだです！");
+const onEnterKey = (): void => {
+  msg.value = "エンターキーが押されました。";
+};
+
+const onRightButtonClick = (): void => {
+  msg.value = "ボタンが右クリックされました。";
+};
+
+const onShiftClick = (): void => {
+  msg.value = "シフトを押しながらクリックされました。";
+};
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey"><br>
+  <button v-on:click.right="onRightButtonClick">右クリック
+  </button><br>
+  <button v-on:click.shift="onShiftClick">シフトを押しながらクリック
+  </button>
+</template>
